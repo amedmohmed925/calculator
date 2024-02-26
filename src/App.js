77c3560bb139028259244e32decfa539bb8a2a98
Ahmed <amedmohmed925.com>
2024-02-26 22:54:44 +0200

    calculator

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import  {Calculator } from './calculator';
+function App() {
+  return (
+    <div className="App">
+      <Calculator />
+    </div>
+  );
+}
+
+export default App;
